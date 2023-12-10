dae4f92dea3be725cb6cd1374ce51b00651988d6
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2023-12-10 14:01:43 +0100

    Initial Commit

---
+++ pmsi-app/pmsi_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pmsi_appApp.swift
+//  pmsi-app
+//
+//  Created by Leonard Pelzer on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct pmsi_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
