1d2f1503998772ec3cf2a83962baaf986b5b65c0
Finn Nils Gedrath <finn.gedrath@gmail.com>
2023-12-12 13:26:05 +0100

    haptic engine

--- pmsi-app/ContentView.swift
+++ pmsi-app/ContentView.swift
@@ -10,39 +10,10 @@ import CoreHaptics
 
 struct ContentView: View {
     
-    
-    @State var engine: CHHapticEngine!
+    @State private var engine: CHHapticEngine?
     @State private var isProgressing = false;
     
-    
-    
     let totalNumOfRounds = 10;
-    
-    func startExperiemnt() {
-        let notifGenerator = UINotificationFeedbackGenerator()
-        
-        Task {
-            isProgressing = true
-            
-            // Code function
-            for round in 1...totalNumOfRounds {
-                
-                let min = 5.0
-                let max = 20.0
-                let timing = Double.random(in: min...max)
-                
-                print("Wait for \(timing) seconds...")
-                
-                try await Task.sleep(nanoseconds: UInt64(timing * Double(NSEC_PER_SEC)))
-                
-                await notifGenerator.notificationOccurred(.success)
-                
-                print("\(round): Run at \(Date())")
-            }
-            
-            isProgressing = false
-        }
-    }
     
     var body: some View {
         HStack {
@@ -57,6 +28,7 @@ struct ContentView: View {
                 Text("Begin")
             }
             .disabled(isProgressing)
+            .onAppear(perform: prepareHaptics)
             
             ProgressView()
                 .padding()
@@ -66,6 +38,98 @@ struct ContentView: View {
         
     }
     
+    func startExperiemnt() {
+        //let notifGenerator = UINotificationFeedbackGenerator()
+        
+        Task {
+            isProgressing = true
+            
+            // Code function
+            for round in 1...totalNumOfRounds {
+                
+                let min = 4.0
+                let max = 4.0
+                let timing = Double.random(in: min...max)
+                
+                print("Wait for \(timing) seconds...")
+                
+                try await Task.sleep(nanoseconds: UInt64(timing * Double(NSEC_PER_SEC)))
+                
+                //await notifGenerator.notificationOccurred(.success)
+                runHaptic()
+                
+                print("\(round): Run at \(Date())")
+            }
+            
+            isProgressing = false
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch let error {
+            fatalError("Engine Creation Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func runHaptic() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(
+                parameterID: .hapticIntensity,
+                value: Float(i) // <= einstellbar
+            )
+            let sharpness = CHHapticEventParameter(
+                parameterID: .hapticSharpness,
+                value: Float(i) // <= einstellbar
+            )
+            let event = CHHapticEvent(
+                eventType: .hapticTransient, // <= einstellbar
+                parameters: [intensity, sharpness],
+                relativeTime: i
+                //duration: 1
+            )
+            
+            events.append(event)
+        }
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(
+                parameterID: .hapticIntensity,
+                value: Float(1 - i) // <= einstellbar
+            )
+            let sharpness = CHHapticEventParameter(
+                parameterID: .hapticSharpness,
+                value: Float(1 - i) // <= einstellbar
+            )
+            let event = CHHapticEvent(
+                eventType: .hapticTransient, // <= einstellbar
+                parameters: [intensity, sharpness],
+                relativeTime: 1 + i
+                //duration: 1
+            )
+            
+            events.append(event)
+        }
+        
+        
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern with \(error.localizedDescription)")
+        }
+    }
 
 }
 
